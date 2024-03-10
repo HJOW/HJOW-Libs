07396aa3f370e8172466a96134760bde1f09d883
허진원 <hujinone11@hotmail.com>
2024-03-11 05:45:13 +0900

    Add creating captcha image method

--- src/hjow/common/util/GUIUtil.java
+++ src/hjow/common/util/GUIUtil.java
@@ -18,21 +18,30 @@ limitations under the License.
 
 package hjow.common.util;
 
+import java.awt.Color;
 import java.awt.Component;
 import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics2D;
 import java.awt.GraphicsEnvironment;
 import java.awt.Toolkit;
 import java.awt.Window;
 import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
+import java.awt.image.BufferedImage;
 import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.ObjectInputStream;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.imageio.ImageIO;
 import javax.swing.UIManager;
 import javax.swing.UIManager.LookAndFeelInfo;
 import javax.swing.plaf.FontUIResource;
@@ -491,5 +500,179 @@ public class GUIUtil
         }
         
         return null;
+    }
+    
+    /** Create captcha image and return base64 code */
+    public static String createImageCaptchaBase64(String code, int width, int height, int noises, int fontSize, boolean darkMode, List<String> fontFamily) throws IOException {
+    	int backr, backg, backb, forer, foreg, foreb;
+    	
+    	if(darkMode) {
+    		backr = 0;
+    		backg = 0;
+    		backb = 0;
+    		forer = 250;
+    		foreg = 250;
+    		foreb = 250;
+    	} else {
+    		backr = 250;
+    		backg = 250;
+    		backb = 250;
+    		forer = 0;
+    		foreg = 0;
+    		foreb = 0;
+    	}
+    	
+    	return createImageCaptchaBase64(code, width, height, noises, fontSize, backr, backg, backb, forer, foreg, foreb, fontFamily);
+    }
+    
+    /** Create captcha image and return base64 code */
+    public static String createImageCaptchaBase64(String code, int width, int height, int noises, int fontSize, int backr, int backg, int backb, int forer, int foreg, int foreb, List<String> fontFamily) throws IOException {
+    	if(code == null) {
+            code = "REFRESH";
+        }
+        
+        if(fontFamily == null) {
+        	fontFamily = new ArrayList<String>();
+        }
+        if(fontFamily.isEmpty()) {
+        	fontFamily.add("Serif");
+        	fontFamily.add("Arial");
+        	fontFamily.add("Helvetica");
+        }
+        
+        BufferedImage image    = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        Graphics2D    graphics = image.createGraphics();
+        
+        if(backr <    0) backr =   0;
+        if(backr >= 255) backr = 255;
+        
+        if(backg <    0) backg =   0;
+        if(backg >= 255) backg = 255;
+        
+        if(backb <    0) backb =   0;
+        if(backb >= 255) backb = 255;
+
+        graphics.setColor(new Color(backr, backg, backb));
+        graphics.fillRect(0, 0, width, height);
+
+        FontMetrics metrics = graphics.getFontMetrics();
+        int fontWidth = metrics.stringWidth(code);
+        int gap       = (width - fontWidth) / (code.length() + 1);
+        int x         = gap;
+        int y         = (height - metrics.getHeight()) / 2 + metrics.getAscent();
+        
+        int calcr, calcg, calcb, colorChangeRange, fontRand, fontFamilyCnt, fontStyle;
+        
+        colorChangeRange = 80;
+        fontFamilyCnt = fontFamily.size();
+        fontStyle = Font.BOLD;
+
+        // 방해물 출력
+        for(int ndx=0; ndx<noises; ndx++) {
+            int x1, y1, x2, y2;
+            x1 = (int) (Math.random() * width);
+            y1 = (int) (Math.random() * height);
+            x2 = x1 + (int) (Math.random() * (width  / 2));
+            y2 = y1 + (int) (Math.random() * (height / 2));
+            
+            if(forer >= 128) calcr = forer - (int) (Math.random() * colorChangeRange);
+            else             calcr = forer + (int) (Math.random() * colorChangeRange);
+            
+            if(forer >= 128) calcg = foreg - (int) (Math.random() * colorChangeRange);
+            else             calcg = foreg + (int) (Math.random() * colorChangeRange);
+            
+            if(forer >= 128) calcb = foreb - (int) (Math.random() * colorChangeRange);
+            else             calcb = foreb + (int) (Math.random() * colorChangeRange);
+            
+            while(Math.abs(backr - calcr) <= 15 && Math.abs(backg - calcg) <= 15 && Math.abs(backb - calcb) <= 15) {
+            	if(forer >= 128) calcr = forer - (int) (Math.random() * colorChangeRange);
+                else             calcr = forer + (int) (Math.random() * colorChangeRange);
+                
+                if(forer >= 128) calcg = foreg - (int) (Math.random() * colorChangeRange);
+                else             calcg = foreg + (int) (Math.random() * colorChangeRange);
+                
+                if(forer >= 128) calcb = foreb - (int) (Math.random() * colorChangeRange);
+                else             calcb = foreb + (int) (Math.random() * colorChangeRange);
+            }
+            
+            if(calcr <    0) calcr =   0;
+            if(calcr >= 255) calcr = 255;
+            
+            if(calcg <    0) calcg =   0;
+            if(calcg >= 255) calcg = 255;
+            
+            if(calcb <    0) calcb =   0;
+            if(calcb >= 255) calcb = 255;
+            
+            graphics.setColor(new Color(calcr, calcg, calcb));
+            graphics.drawLine(x1, y1, x2, y2);
+        }
+
+        // 글자 출력
+        for(int idx=0; idx<code.length(); idx++) {
+            char charNow = code.charAt(idx);
+            
+            if(forer >= 128) calcr = forer - (int) (Math.random() * colorChangeRange);
+            else             calcr = forer + (int) (Math.random() * colorChangeRange);
+            
+            if(forer >= 128) calcg = foreg - (int) (Math.random() * colorChangeRange);
+            else             calcg = foreg + (int) (Math.random() * colorChangeRange);
+            
+            if(forer >= 128) calcb = foreb - (int) (Math.random() * colorChangeRange);
+            else             calcb = foreb + (int) (Math.random() * colorChangeRange);
+            
+            while(Math.abs(backr - calcr) <= 15 && Math.abs(backg - calcg) <= 15 && Math.abs(backb - calcb) <= 15) {
+            	if(forer >= 128) calcr = forer - (int) (Math.random() * colorChangeRange);
+                else             calcr = forer + (int) (Math.random() * colorChangeRange);
+                
+                if(forer >= 128) calcg = foreg - (int) (Math.random() * colorChangeRange);
+                else             calcg = foreg + (int) (Math.random() * colorChangeRange);
+                
+                if(forer >= 128) calcb = foreb - (int) (Math.random() * colorChangeRange);
+                else             calcb = foreb + (int) (Math.random() * colorChangeRange);
+            }
+            
+            if(calcr <    0) calcr =   0;
+            if(calcr >= 255) calcr = 255;
+            
+            if(calcg <    0) calcg =   0;
+            if(calcg >= 255) calcg = 255;
+            
+            if(calcb <    0) calcb =   0;
+            if(calcb >= 255) calcb = 255;
+            
+            graphics.setColor(new Color(calcr, calcg, calcb));
+            
+            int nowX = x + metrics.charWidth(charNow) / 2;
+            int ang  = ((int) (Math.random() * 41)) - 20;
+            
+            graphics.rotate(Math.toRadians(ang), nowX, y);
+            
+            fontRand = (int) Math.floor(Math.random() * fontFamilyCnt);
+            if(fontRand >= fontFamilyCnt) fontRand = fontFamilyCnt - 1;
+            if(fontRand < 0) fontRand = 0;
+            
+            if(Math.random() >= 0.5) fontStyle = Font.BOLD;
+            else                     fontStyle = Font.BOLD | Font.ITALIC;
+            
+            graphics.setFont(new Font(fontFamily.get(fontRand), fontStyle, fontSize + ((int) Math.random() * 4) - 2));
+            graphics.drawString(String.valueOf(charNow), nowX, y + ((int) ((Math.random() * height) / 3.0)));
+            graphics.rotate(Math.toRadians(ang) * (-1), nowX, y);
+            
+            x += metrics.charWidth(charNow) + gap;
+        }
+
+        ByteArrayOutputStream binary = new ByteArrayOutputStream();
+        ImageIO.write(image, "jpg", binary);
+        image    = null;
+        graphics = null;
+
+        String bs64str = SecurityUtil.base64String(binary.toByteArray());
+        binary = null;
+        return bs64str;
+    }
+    
+    public static String createTextCaptcha(String code) {
+    	return code;
     }
 }
